fb4c5c28f61d21503c0e2bb6a593d57fb9dc649b
Xavi Ablaza <25378966+xaviablaza@users.noreply.github.com>
2022-05-20 15:29:35 +0800

    Add FleetApp::Server::BrooceCommand.create

---
+++ src/fleet_app/brooce_command/create.cr
@@ -0,0 +1,10 @@
+module FleetApp
+  class Server
+    class BrooceCommand
+      def self.create(host : String, game_name : String, server_id : String, body : String = "")
+        client = FleetApp::Client.new(host)
+        client.post("/api/v1/#{game_name}/servers/#{server_id}/brooce_command", body)
+      end
+    end
+  end
+end
