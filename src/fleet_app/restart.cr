3bef1a615acbac1fa1991956ddbaf9c31ad55fe1
paula4230 <paudepolonia000@yahoo.com>
2022-06-15 17:45:10 +0800

    Check for environment

--- src/fleet_app/restart.cr
+++ src/fleet_app/restart.cr
@@ -1,7 +1,12 @@
 module FleetApp
   class Server
-    def self.restart(host : String, game_name : String, server_id : String, body : String = "")
-      client = FleetApp::Client.new
+    def self.restart(host : String, game_name : String, server_id : String, body : String = "", environment : String = "production")
+      if environment == "production"
+        client = FleetApp::Client.new
+      else
+        client = FleetApp::Client.new("sandbox-fleet.hostari.com")
+      end
+
       client.post("/api/v1/#{game_name}/servers/#{server_id}/restart?queue_name=#{host}", body)
     end
   end
