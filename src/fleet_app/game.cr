19210b083d729c0aee3fc05eb8428ec37052c1cd
Weltinio <68684063+Weltinio@users.noreply.github.com>
2022-09-07 04:16:16 +0800

    Revert "Revert "add project zomboid""

--- src/fleet_app/game.cr
+++ src/fleet_app/game.cr
@@ -1,13 +1,15 @@
 module FleetApp
   module Game
-    VALHEIM     = "valheim"
-    CORE_KEEPER = "core_keeper"
-    V_RISING    = "v_rising"
+    VALHEIM         = "valheim"
+    CORE_KEEPER     = "core_keeper"
+    V_RISING        = "v_rising"
+    PROJECT_ZOMBOID = "project_zomboid"
 
     LIST = [
       VALHEIM,
       CORE_KEEPER,
       V_RISING,
+      PROJECT_ZOMBOID,
     ]
   end
 end
