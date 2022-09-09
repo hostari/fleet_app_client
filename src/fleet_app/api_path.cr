170bfb21e688d97794514f5ab4efe0ef7d6a34c1
Manuel Tinio <mrctinio@gmail.com>
2022-09-09 15:22:05 +0800

    fix conditional for api_path

--- src/fleet_app/api_path.cr
+++ src/fleet_app/api_path.cr
@@ -5,14 +5,18 @@ module FleetApp
     API_VERSION = "v1"
 
     def initialize(game_name : String, server_id : String, host : String, username : String = "", action : String = "")
-      if action == "" && username == ""
-        @path = "/api/#{API_VERSION}/#{game_name}/servers/#{server_id}?queue_name=#{host}"
-      elsif action == "" && !username == ""
-        @path = "/api/#{API_VERSION}/#{game_name}/servers/#{server_id}?queue_name=#{host}&username=#{username}"
-      elsif !action == "" && !username == ""
-        @path = "/api/#{API_VERSION}/#{game_name}/servers/#{server_id}/#{action}?queue_name=#{host}&username=#{username}"
-      elsif !action == "" && username == ""
-        @path = "/api/#{API_VERSION}/#{game_name}/servers/#{server_id}/#{action}?queue_name=#{host}"
+      if action == ""
+        if username == ""
+          @path = "/api/#{API_VERSION}/#{game_name}/servers/#{server_id}?queue_name=#{host}"
+        else
+          @path = "/api/#{API_VERSION}/#{game_name}/servers/#{server_id}?queue_name=#{host}&username=#{username}"
+        end
+      else
+        if username == ""
+          @path = "/api/#{API_VERSION}/#{game_name}/servers/#{server_id}/#{action}?queue_name=#{host}"
+        else
+          @path = "/api/#{API_VERSION}/#{game_name}/servers/#{server_id}/#{action}?queue_name=#{host}&username=#{username}"
+        end
       end
     end
   end
