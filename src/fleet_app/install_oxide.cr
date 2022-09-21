a9c18c1631b64b3a0c0ef605052488d3b3f69b46
Manuel Tinio <mrctinio@gmail.com>
2022-09-21 14:20:21 +0800

    add update and install_oxide

---
+++ src/fleet_app/install_oxide.cr
@@ -0,0 +1,41 @@
+module FleetApp
+  class Server
+    # Installs Oxide on a rust server.
+    #
+    # This is different from creating a game server. Starting a game server assumes that the
+    # server has already been created, and is starting from a stopped state.
+    #
+    # Use `host` to specify the queue name in brooce to send to.
+    # For example, use `2013191.xyz` if the command should be executed on tms1.
+    # If you don't know which queue to send to, the SRE lead should be able to advise you
+    # based on how they configured brooce.
+    #
+    # Use `game_name` to specify the game you are sending a brooce command for.
+    # See: `FleetApp::Game` for possible values.
+    #
+    # Use `server_id` to specify the internal Hostari id. Can be a `subscription_id` from GamesSite,
+    # or a `server_id` from VSH.
+    #
+    # `body` is an optional, JSON-formatted string that is sent as the request body which defaults to an empty string.
+    # This is used to override the default command in fleet app.
+    # For example, you can send this: `{command: "echo 'foobar'"}.to_json`
+    #
+    # `environment` is an optional string that specifies which fleet app to send the request to.
+    def self.install_oxide(host : String, game_name : String, server_id : String, body : String = "", environment : String = "production", username : String = "")
+      FleetApp::ClientWrapper.new(environment).post(
+        game_name: game_name,
+        path: ApiPath.new(game_name, server_id, host, username: username, action: "install_oxide").path,
+        body: body
+      )
+    end
+
+    def self.install_oxide_with_auth(host : String, game_name : String, server_id : String, basic_auth : String, body : String = "", environment : String = "production", username : String = "")
+      FleetApp::ClientWrapper.new(environment).post_with_auth(
+        game_name: game_name,
+        path: ApiPath.new(game_name, server_id, host, username: username, action: "install_oxide").path,
+        body: body,
+        basic_auth: basic_auth
+      )
+    end
+  end
+end
