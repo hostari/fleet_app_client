aedcad8286bd056241c7e3e168621fdce9fa3129
Manuel Tinio <mrctinio@gmail.com>
2022-09-22 14:48:11 +0800

    update shard version

--- src/fleet_app_client.cr
+++ src/fleet_app_client.cr
@@ -2,7 +2,7 @@ require "json"
 require "http/client"
 
 class FleetAppClient
-  VERSION      = "0.1.9"
+  VERSION      = "0.2.0"
   HOST         = "fleet.hostari.com"
   SANDBOX_HOST = "sandbox-#{HOST}"
 end
