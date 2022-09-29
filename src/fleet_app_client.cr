0ec8ec4118027259acadd9ec9818c28d12110ce3
Manuel Tinio <mrctinio@gmail.com>
2022-09-29 20:21:30 +0800

    update version

--- src/fleet_app_client.cr
+++ src/fleet_app_client.cr
@@ -2,7 +2,7 @@ require "json"
 require "http/client"
 
 class FleetAppClient
-  VERSION      = "0.2.1"
+  VERSION      = "0.2.2"
   HOST         = "fleet.hostari.com"
   SANDBOX_HOST = "sandbox-#{HOST}"
 end
