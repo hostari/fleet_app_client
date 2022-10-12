16e62d889ebc42849bb52d9094867e4b4a8894fd
Manuel Tinio <mrctinio@gmail.com>
2022-10-12 13:25:46 +0800

    add terraria and update shard

--- src/fleet_app_client.cr
+++ src/fleet_app_client.cr
@@ -2,7 +2,7 @@ require "json"
 require "http/client"
 
 class FleetAppClient
-  VERSION      = "0.2.2"
+  VERSION      = "0.2.3"
   HOST         = "fleet.hostari.com"
   SANDBOX_HOST = "sandbox-#{HOST}"
 end
